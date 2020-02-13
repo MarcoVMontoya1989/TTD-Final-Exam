cf8dd1b8d15309c66959dbcac1d31deac9ed465f
Marco Antonio V Montoya Cardenas <marcovmontoya@gmail.com>
2020-02-13 10:08:19 -0500

    solution

---
+++ assets/scripts/app.js
@@ -0,0 +1,52 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
+
+function sendHttpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error('Something went wrong!'));
+      }
+    };
+
+    xhr.onerror = function() {
+      reject(new Error('Failed to send request!'));
+    };
+
+    xhr.send(JSON.stringify(data));
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://api.github.com/users'
+    );
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.login.toUpperCase();
+      postEl.querySelector('p').textContent = post.html_url;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+fetchButton.addEventListener('click', fetchPosts);
