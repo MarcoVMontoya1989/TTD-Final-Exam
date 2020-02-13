f29bde213003383dfd54c0d1856988713bdbccc2
Marco Antonio V Montoya Cardenas <marcovmontoya@gmail.com>
2020-02-13 10:57:54 -0500

    add file local

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,17 +2,24 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const localButton = document.querySelector('#local');
 const postList = document.querySelector('ul');
 
-function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
+function sendHttpRequest(method, url, flag ,data) {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
     xhr.open(method, url);
 
+    if(flag === 'local') {
+      // xhr.open(method, url, false);
+    } else {
+      // xhr.open(method, url);
+    }
+
     xhr.responseType = 'json';
 
-    xhr.onload = function() {
+    xhr.onload = function () {
       if (xhr.status >= 200 && xhr.status < 300) {
         resolve(xhr.response);
       } else {
@@ -20,33 +27,53 @@ function sendHttpRequest(method, url, data) {
       }
     };
 
-    xhr.onerror = function() {
+    xhr.onerror = function () {
       reject(new Error('Failed to send request!'));
     };
 
     xhr.send(JSON.stringify(data));
   });
+}
 
-  return promise;
+async function fetchPostsLocal() {
+  const path = 'file://../File/github.json';
+
+  try {
+    const responseData = await sendHttpRequest('GET', path, 'local');
+    const listOfPosts = responseData;
+
+    renderList(listOfPosts);
+
+  } catch (error) {
+    alert(`${error.message}`);
+  }
 }
 
 async function fetchPosts() {
   try {
     const responseData = await sendHttpRequest(
-      'GET',
-      'https://api.github.com/users'
+        'GET',
+        'https://api.github.com/users',
+        'api'
     );
     const listOfPosts = responseData;
-    for (const post of listOfPosts) {
-      const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector('h2').textContent = post.login.toUpperCase();
-      postEl.querySelector('p').textContent = post.html_url;
-      postEl.querySelector('li').id = post.id;
-      listElement.append(postEl);
-    }
+
+    renderList(listOfPosts);
+
   } catch (error) {
-    alert(error.message);
+    alert(`${error.message} Please use the Local Button`);
+  }
+}
+
+function renderList(listOfPosts) {
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.login.toUpperCase();
+    postEl.querySelector('p').textContent = post.html_url;
+    postEl.querySelector('li').id = post.id;
+    listElement.append(postEl);
   }
 }
 
 fetchButton.addEventListener('click', fetchPosts);
+localButton.addEventListener('click', fetchPostsLocal);
